430a7c8d630a65120b1b8e96ef6dffa2eb39d661
Ce Liu <lc6866@outlook.com>
2026-02-08 18:00:20 +0800

    Update atomic forces regex patterns for parsing

--- cp2kdata/block_parser/forces.py
+++ cp2kdata/block_parser/forces.py
@@ -1,7 +1,7 @@
 import regex as re
 import numpy as np
 
-ATOMIC_FORCES_RE = re.compile(
+ATOMIC_FORCES_RE_OLD = re.compile(
     r"""
     \sATOMIC\sFORCES\sin\s\[a\.u\.\]\s*\n
     \n
@@ -19,14 +19,38 @@ ATOMIC_FORCES_RE = re.compile(
     re.VERBOSE
 )
 
+ATOMIC_FORCES_RE_NEW = re.compile(
+    r"""
+    \sFORCES\|\sAtomic\sforces\s\[.+?\]\s*\n
+    \sFORCES\|\s+Atom\s+x\s+y\s+z\s+\|f\|\s*\n
+    (
+        \sFORCES\|\s+
+        (?P<atom>\d+)
+        \s+(?P<x>[\d\.\-E\+]+)
+        \s+(?P<y>[\d\.\-E\+]+)
+        \s+(?P<z>[\d\.\-E\+]+)
+        \s+[\d\.\-E\+]+
+        \n
+    )+
+    """,
+    re.VERBOSE
+)
+
 
 def parse_atomic_forces_list(output_file):
     atomic_forces_list = []
-    for match in ATOMIC_FORCES_RE.finditer(output_file):
+    
+    matches = list(ATOMIC_FORCES_RE_OLD.finditer(output_file))
+
+    if not matches:
+        matches = list(ATOMIC_FORCES_RE_NEW.finditer(output_file))
+
+    for match in matches:
         atomic_forces = []
         for x, y, z in zip(*match.captures("x", "y", "z")):
             atomic_forces.append([x, y, z])
         atomic_forces_list.append(atomic_forces)
+
     if atomic_forces_list:
         return np.array(atomic_forces_list, dtype=float)
     else:
